ec3bb9e2b4dd376cadc9fdab697dda4906d10c61
JAYJAYTGG <155000328+JJTGG@users.noreply.github.com>
2026-09-12 18:32:10 +0100

    feat: add AUREN featured collection

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import CategoryStrip from "@/components/home/CategoryStrip";
+import FeaturedCollection from "@/components/home/FeaturedCollection";
 import Hero from "@/components/home/Hero";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main>
       <Hero />
       <CategoryStrip />
+      <FeaturedCollection />
     </main>
   );
 }
